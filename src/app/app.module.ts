21741c3b8c5347d74021424f5ac2ccb3d84df3ec
SelenaMilutin <selena.milutin@gmail.com>
2022-11-30 18:08:19 +0100

    Add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './infrastructure/material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot([
+      {
+        path: "components\login",
+        component: LoginComponent
+      }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/login-authentification.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginAuthentificationService {
+
+  constructor() { }
+
+  getUserDetails(){
+    //post details to api and check
+  }
+}
