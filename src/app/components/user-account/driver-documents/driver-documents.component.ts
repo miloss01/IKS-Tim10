f0b2a922a3488ad38670f45392d3e15dac274455
SelenaMilutin <selena.milutin@gmail.com>
2022-12-08 22:28:13 +0100

    Add vechicle component

--- src/app/components/user-account/driver-documents/driver-documents.component.ts
+++ src/app/components/user-account/driver-documents/driver-documents.component.ts
@@ -5,7 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './driver-documents.component.html',
   styleUrls: ['./driver-documents.component.css']
 })
+
+
 export class DriverDocumentsComponent implements OnInit {
+  //documents: Array(Documet);
 
   constructor() { }
 
--- src/app/components/user-account/driver-vechicle/driver-vechicle.component.ts
+++ src/app/components/user-account/driver-vechicle/driver-vechicle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-driver-vechicle',
@@ -7,12 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverVechicleComponent implements OnInit {
   imageSrc: string;
+  averageGrade: number;
+  carName: string;
+  lisencePlate: string;
+  numberOfSeats: number;
+  babySeat: boolean;
+  petsAllowed: boolean;
+  changingInformationForm = new FormGroup({
+    carName: new FormControl,
+    lisencePlate: new FormControl(),
+    numberOfSeats: new FormControl(),
+  })
 
   constructor() {
     this.imageSrc = "https://www.magazinauto.com/wp-content/uploads/2020/09/IMG_3305-e1600082024266-680x365.jpg";
+    this.averageGrade = 4.5;
+    this.carName = "Honda Civic V59";
+    this.lisencePlate = "599 23-IO"
+    this.numberOfSeats = 5;
+    this.babySeat = true;
+    this.petsAllowed = false;
+    this.changingInformationForm.setValue({
+      carName: this.carName,
+      lisencePlate: this.lisencePlate,
+      numberOfSeats: this.numberOfSeats,
+    });
    }
+
+  
 
   ngOnInit(): void {
   }
+
+  submitChanges(): void{}
 
 }
