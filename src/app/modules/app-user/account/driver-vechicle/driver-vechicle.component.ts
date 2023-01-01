d61f65839b73c4627e20d35f8be9bb5c41c1fd16
unknown <spremic.katarina@gmail.com>
2023-01-01 19:39:25 +0100

    Add eslint and refactor account package

--- src/app/modules/app-user/account/driver-vechicle/driver-vechicle.component.ts
+++ src/app/modules/app-user/account/driver-vechicle/driver-vechicle.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { LoginAuthentificationService } from 'src/app/modules/auth/service/login-authentification.service';
-import { ManageDriversService } from '../../manage-drivers/service/manage-drivers.service';
-import { Vehicle } from 'src/app/models/models';
-import { UserServiceService } from '../services/user.service';
-import { Output, EventEmitter } from '@angular/core';
-import { MatCheckboxChange } from '@angular/material/checkbox';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { LoginAuthentificationService } from 'src/app/modules/auth/service/login-authentification.service'
+import { ManageDriversService } from '../../manage-drivers/service/manage-drivers.service'
+import { Vehicle } from 'src/app/models/models'
+import { UserServiceService } from '../services/user.service'
+import { MatCheckboxChange } from '@angular/material/checkbox'
 
 @Component({
   selector: 'app-driver-vechicle',
@@ -14,8 +15,8 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
   styleUrls: ['./driver-vechicle.component.css']
 })
 export class DriverVechicleComponent implements OnInit {
-  @Output() vehicleEvent = new EventEmitter<Vehicle>();
-  vehicle:Vehicle = {
+  @Output() vehicleEvent = new EventEmitter<Vehicle>()
+  vehicle: Vehicle = {
     id: 0,
     driverId: 0,
     vehicleType: '',
@@ -30,66 +31,67 @@ export class DriverVechicleComponent implements OnInit {
     babyTransport: false,
     petTransport: false
   }
+
   changingInformationForm = new FormGroup({
-    carName: new FormControl,
+    carName: new FormControl(),
     lisencePlate: new FormControl(),
-    numberOfSeats: new FormControl(),
+    numberOfSeats: new FormControl()
   })
 
-  constructor(
-    private route:ActivatedRoute,
-    private userService: UserServiceService,
-    private manageDrivers: ManageDriversService,
-    private userAuthentificationService: LoginAuthentificationService) {
-    }
-
+  constructor (
+    private readonly route: ActivatedRoute,
+    private readonly userService: UserServiceService,
+    private readonly manageDrivers: ManageDriversService,
+    private readonly userAuthentificationService: LoginAuthentificationService) {
+  }
 
   // Is component shown for driver viewing their own account
-  isDriver = true;
-  
+  isDriver = true
 
-  ngOnInit(): void {
-    if (this.userAuthentificationService.getRole() == 2) {
+  ngOnInit (): void {
+    if (this.userAuthentificationService.getRole() === 2) {
       this.userService.selectedValue$.subscribe((value) => {
         this.route.params.subscribe((params) => {
           this.userService
-          .getVehicleById(value)
-          .subscribe((fetchedVechicle:Vehicle) => {
-            this.vehicle =fetchedVechicle; 
-            console.log("DRIVER VEHICLE COMPONENT - Fetched Vehicle for User with id " + value +", " + JSON.stringify(fetchedVechicle));})
-        });
+            .getVehicleById(value)
+            .subscribe((fetchedVechicle: Vehicle) => {
+              this.vehicle = fetchedVechicle
+              console.log('DRIVER VEHICLE COMPONENT - Fetched Vehicle for User with id ' + value + ', ' + JSON.stringify(fetchedVechicle))
+            })
+        })
       })
-      this.isDriver = false;
+      this.isDriver = false
     } else {
       this.route.params.subscribe((params) => {
         this.userService
-        .getVehicleById(this.userAuthentificationService.getId())
-        .subscribe((fetchedVechicle:Vehicle) => {
-          this.vehicle =fetchedVechicle; 
-          console.log("DRIVER VEHICLE COMPONENT - Fetched Vehicle for User with id " + this.userAuthentificationService.getId() +", " + JSON.stringify(fetchedVechicle));})
-      });
+          .getVehicleById(this.userAuthentificationService.getId())
+          .subscribe((fetchedVechicle: Vehicle) => {
+            this.vehicle = fetchedVechicle
+            console.log('DRIVER VEHICLE COMPONENT - Fetched Vehicle for User with id ' + this.userAuthentificationService.getId() + ', ' + JSON.stringify(fetchedVechicle))
+          })
+      })
     }
   }
 
-  toggle(event: MatCheckboxChange){
-    console.log(event.source.checked);
-    this.vehicle.babyTransport = event.source.checked;
+  toggle (event: MatCheckboxChange): void {
+    console.log(event.source.checked)
+    this.vehicle.babyTransport = event.source.checked
   }
 
-  toggle2(event: MatCheckboxChange){
-    this.vehicle.petTransport = event.source.checked;
+  toggle2 (event: MatCheckboxChange): void {
+    this.vehicle.petTransport = event.source.checked
   }
 
-  saveChanges(): void {
-    if (this.changingInformationForm.get('carName')?.value){
-      this.vehicle.model = this.changingInformationForm.get('carName')?.value;
+  saveChanges (): void {
+    if (this.changingInformationForm.get('carName')?.value) {
+      this.vehicle.model = this.changingInformationForm.get('carName')?.value
     }
-    if (this.changingInformationForm.get('lisencePlate')?.value){
-      this.vehicle.licenseNumber = this.changingInformationForm.get('lisencePlate')?.value;    }
-    if (this.changingInformationForm.get('numberOfSeats')?.value){
-      this.vehicle.passengerSeats = this.changingInformationForm.get('numberOfSeats')?.value;
+    if (this.changingInformationForm.get('lisencePlate')?.value) {
+      this.vehicle.licenseNumber = this.changingInformationForm.get('lisencePlate')?.value
     }
-    this.vehicleEvent.emit(this.vehicle);
+    if (this.changingInformationForm.get('numberOfSeats')?.value) {
+      this.vehicle.passengerSeats = this.changingInformationForm.get('numberOfSeats')?.value
+    }
+    this.vehicleEvent.emit(this.vehicle)
   }
-  
 }
--- src/app/modules/app-user/account/services/user.service.ts
+++ src/app/modules/app-user/account/services/user.service.ts
@@ -1,39 +1,42 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { DocumentDTO, Vehicle } from 'src/app/models/models';
-import { environment } from 'src/environments/environment';
-import { AppUser } from '../basic-user-information/basic-user-information.component';
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/prefer-readonly */
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
+import { DocumentDTO, Vehicle } from 'src/app/models/models'
+import { environment } from 'src/environments/environment'
+import { AppUser } from '../basic-user-information/basic-user-information.component'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-  private value$ = new BehaviorSubject<any>({});
-  selectedValue$ = this.value$.asObservable();
+  private value$ = new BehaviorSubject<any>({})
+  selectedValue$ = this.value$.asObservable()
 
-  constructor(private http: HttpClient) { }
+  constructor (private http: HttpClient) { }
 
-  setValue(test: any) {
-    this.value$.next(test);
+  setValue (test: any): void {
+    this.value$.next(test)
   }
 
-  getUser(id:number): Observable<AppUser> {
-    return this.http.get<AppUser>(environment.apiHost + "passenger/" + id);
-  }
-  
-  getVechicle(id:number):Observable<Vehicle> {
-    return this.http.get<Vehicle>(environment.apiHost + "driver/" + id + "/vehicle");
+  getUser (id: number): Observable<AppUser> {
+    return this.http.get<AppUser>(environment.apiHost + "passenger/" + id)
   }
 
-  getVehicleById(id: number):Observable<Vehicle> {
-    return this.http.get<Vehicle>(environment.apiHost + "driver/"+ id +"/vehicle");
+  getVechicle (id: number): Observable<Vehicle> {
+    return this.http.get<Vehicle>(environment.apiHost + "driver/" + id + "/vehicle")
   }
 
-  saveChanges(user: AppUser, id:number): Observable<any> {
+  getVehicleById (id: number): Observable<Vehicle> {
+    return this.http.get<Vehicle>(environment.apiHost + "driver/" + id + "/vehicle")
+  }
+
+  saveChanges (user: AppUser, id: number): Observable<any> {
     const options: any = {
-      responseType: 'text',
-    };
+      responseType: 'text'
+    }
 
     return this.http.put<string>(
       environment.apiHost + 'passenger/' + id,
@@ -46,21 +49,20 @@ export class UserServiceService {
         profilePicture: user.profilePicture
       },
       options
-    );
+    )
   }
 
-  getDriverDocuments(id:number): Observable<DocumentDTO[]> {
-    return this.http.get<DocumentDTO[]>(environment.apiHost + 'driver/' + id + '/documents');
+  getDriverDocuments (id: number): Observable<DocumentDTO[]> {
+    return this.http.get<DocumentDTO[]>(environment.apiHost + 'driver/' + id + '/documents')
   }
 
-  getUserById(id: number): Observable<AppUser> {
-    let params = new HttpParams();
-    params = params.append('id', id); 
-    return this.http.get<AppUser>(environment.apiHost + "user/1", {params: params});
+  getUserById (id: number): Observable<AppUser> {
+    let params = new HttpParams()
+    params = params.append('id', id)
+    return this.http.get<AppUser>(environment.apiHost + "user/1", { params })
   }
 
-  getDriverById(id: number): Observable<AppUser> {
-    return this.http.get<AppUser>(environment.apiHost + "driver/" + id);
+  getDriverById (id: number): Observable<AppUser> {
+    return this.http.get<AppUser>(environment.apiHost + "driver/" + id)
   }
-
 }
